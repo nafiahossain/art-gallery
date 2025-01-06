907ea2f2c6cf6353c8a5ac7edeab2440e5333f05
Nafia Hossain <66172909+nafiahossain@users.noreply.github.com>
2025-01-07 01:38:53 +0600

    Reupload

---
+++ Admin/includes/navbar.php
@@ -0,0 +1,108 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="d-flex align-items-center justify-content-center" href="index.php">
+        <div>
+            <img src="img/logoo.png" alt="logo">
+        </div>
+        <div class="sidebar-brand ">Admin <br> Panel</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Components
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="admin.php">Admins</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_artist.php">Artists</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_users.php">Users</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_artworks.php">Artworks</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_gallery.php">Galleries</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_shows.php">Shows</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_reviews.php">Reviews</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_contact.php">Contact Us Messages</a>
+    </li>
+
+    
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+
+
+</ul>
+<!-- End of Sidebar -->
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+
+            
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+
+                <div class="topbar-divider d-none d-sm-block"></div>
+
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hello Admin! </span>
+                    </a>
+                </li>
+
+            </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
---
+++ Admin/includes/scripts.php
@@ -0,0 +1,20 @@
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="js/demo/chart-area-demo.js"></script>
+<script src="js/demo/chart-pie-demo.js"></script>
